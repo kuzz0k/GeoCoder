05d680aa108a50d2ba7755f6eba2bf5ea3432218
kuzz0k <kuznecov00900@yandex.ru>
2025-02-13 15:56:34 +0300

    add marker

--- src/components/Earth/Globe.jsx
+++ src/components/Earth/Globe.jsx
@@ -22,17 +22,18 @@ const Globe = () => {
     if (isDragging.current) return;
     const globe = globeRef.current;
     if (!globe) return;
-
+  
     const intersect = event.intersections[0];
     if (!intersect) return;
-
-    const point = intersect.point.clone().normalize();
-    const lat = Math.asin(point.y) * (180 / Math.PI);
-    const lon = Math.atan2(point.z, point.x) * (180 / Math.PI)*(-1);
-
-    setMarker({ position: point, lat, lon });
-    console.log(lat, lon)
+  
+    const point = intersect.point.clone().normalize().multiplyScalar(2.5*scale);
+    const lat = Math.asin(point.y / 2.5*scale) * (180 / Math.PI);
+    const lon = Math.atan2(point.z, point.x) * (180 / Math.PI) * (-1);
+  
+    setMarker({ position: [point.x, point.y, point.z], lat, lon });
+    console.log("Latitude:", lat, "Longitude:", lon);
   };
+  
 
 
 
@@ -51,15 +52,25 @@ const Globe = () => {
   }, [scale]);
 
   return (
-    <mesh
-      ref={globeRef}
-      onPointerDown={handlePointerDown}
-      onPointerMove={handlePointerMove}
-      onPointerUp={handlePointerUp}
-    >
-      <sphereGeometry args={[2.5, 64, 64]} />
-      <meshStandardMaterial map={texture} />
-    </mesh>
+    <>
+      <mesh
+        ref={globeRef}
+        onPointerDown={handlePointerDown}
+        onPointerMove={handlePointerMove}
+        onPointerUp={handlePointerUp}
+      >
+        <sphereGeometry args={[2.5, 64, 64]} />
+        <meshStandardMaterial map={texture} />
+      </mesh>
+      {marker && (
+        <>
+          <mesh position={marker.position}>
+            <sphereGeometry args={[0.05, 32, 32]} />
+            <meshStandardMaterial color="yellow" />
+          </mesh>
+        </>
+      )}
+    </>
   );
 };
 
